c88e322e21c9bb2fdaf8ad2d7b3ca5c43852b556
Kushal Das <61356005+Kushal997-das@users.noreply.github.com>
2020-09-21 18:55:17 +0530

    introduction
    problem link-https://www.hackerrank.com/challenges/java-stdin-stdout/problem

---
+++ introduction/Java Stdin and Stdout II.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        
+
+        // Write your code here.
+        double d=scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        //String s=scan.next();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
