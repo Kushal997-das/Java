1d67a5854549ae809bc75c8fe1b43d1ee5b83ff0
Tawfik Yasser <tawfekyassertawfek@gmail.com>
2021-01-22 17:08:23 +0200

    Update and rename Java Int to String.java to JavaInttoString.java

--- introduction/JavaInttoString.java
+++ introduction/JavaInttoString.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main {
+public class JavaInttoString {
     public static void main(String[] args) {
        //Write your code here
        String s =Integer.toString(n);
