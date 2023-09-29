28b7e8751835b1888fafe3ddb4bfc3a42bc48d77
Arpan Basu <95604074+ArpanLeedan@users.noreply.github.com>
2023-09-29 20:57:36 +0530

    Update JavaInttoString.java

--- introduction/JavaInttoString.java
+++ introduction/JavaInttoString.java
@@ -2,6 +2,10 @@ import java.util.*;
 public class JavaInttoString {
     public static void main(String[] args) {
        //Write your code here
-       String s =Integer.toString(n);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Integer Value");
+        int num=sc.nextInt();
+       String s =Integer.toString(num);
+        System.out.println("Output is "+s);
     }
 }
